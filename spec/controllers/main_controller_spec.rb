73b5c9564a6f0c726ddafabd7de9767b9b4f72a2
Valentyn_Skrypnyk <vskrypnyk@vskrypnyk.local>
2021-07-06 20:59:42 +0300

    Run Rubocop

--- spec/controllers/main_controller_spec.rb
+++ spec/controllers/main_controller_spec.rb
@@ -1,7 +1,8 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe MainController, type: :controller do
-
   it 'first test' do
     get :index
     expect(response).to have_http_status(200)
